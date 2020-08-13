65afb28da6dddec6348758bfc28f84d4476731bf
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-08-12 23:53:09 -0300

    Updates

---
+++ precache-manifest.5ae8ddb4a4d755e24b65092b1add41fc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790254fa51442e583c0e7751b70a1c6f",
+    "url": "/brainflix-deploy/index.html"
+  },
+  {
+    "revision": "16c7fc5e29306347004d",
+    "url": "/brainflix-deploy/static/css/main.46a26941.chunk.css"
+  },
+  {
+    "revision": "a4394d600427774bded7",
+    "url": "/brainflix-deploy/static/js/2.ed97a801.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brainflix-deploy/static/js/2.ed97a801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c7fc5e29306347004d",
+    "url": "/brainflix-deploy/static/js/main.cecf6a6c.chunk.js"
+  },
+  {
+    "revision": "b362e0cb479ef8f319b7",
+    "url": "/brainflix-deploy/static/js/runtime-main.d31306b0.js"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/brainflix-deploy/static/media/AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/brainflix-deploy/static/media/AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/brainflix-deploy/static/media/AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "03b74b4c4dc7cd48eee599a4517f1441",
+    "url": "/brainflix-deploy/static/media/Icon-fullscreen.03b74b4c.svg"
+  },
+  {
+    "revision": "23c3f8579420d52ffd5c13ad742baa6a",
+    "url": "/brainflix-deploy/static/media/Icon-likes.23c3f857.svg"
+  },
+  {
+    "revision": "a3c51b76d2b01afe928cef3c6b522f21",
+    "url": "/brainflix-deploy/static/media/Icon-play.a3c51b76.svg"
+  },
+  {
+    "revision": "286069001111ea3ed233483011ce0c09",
+    "url": "/brainflix-deploy/static/media/Icon-search.28606900.svg"
+  },
+  {
+    "revision": "909b8976a003b06080b276c94fcfe418",
+    "url": "/brainflix-deploy/static/media/Icon-upload.909b8976.svg"
+  },
+  {
+    "revision": "a488fc5b5ba8a1b910c6b8278131ffac",
+    "url": "/brainflix-deploy/static/media/Icon-views.a488fc5b.svg"
+  },
+  {
+    "revision": "d17c2fe7a1d599b46d1ce25f73786da2",
+    "url": "/brainflix-deploy/static/media/Icon-volume.d17c2fe7.svg"
+  },
+  {
+    "revision": "1e121e6bb74d3a6c39ed045da009ca40",
+    "url": "/brainflix-deploy/static/media/Logo-brainflix.1e121e6b.svg"
+  }
+]);
